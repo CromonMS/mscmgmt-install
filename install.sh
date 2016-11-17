0d18eab87ea9dc1d7ad798d6abbbacfa69a9dc05
Eugene <CromonMS@users.noreply.github.com>
2016-11-17 12:18:31 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-bash
-
 # Installs dependencies for using mscmgmt
 #
 # 1. RVM -> to control Ruby versions.
@@ -17,6 +15,7 @@ if [ ! -d "$HOME/.rvm" ]; then
     gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
     \curl -sSL https://get.rvm.io | bash -s master --ruby=2.3.2
     echo "RVM is Installed using $RUBY_VERSION"
+    bash
     source $HOME/.rvm/scripts/rvm
 else
     echo "RVM is installed"
@@ -24,6 +23,8 @@ fi
 
 gem install rake
 gem install bundler
+
+exit
 
 if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing YADR for the first time"
@@ -34,4 +35,4 @@ if [ ! -d "$HOME/.yadr" ]; then
 else
     echo "YADR is already installed"
 fi
-
+bash
